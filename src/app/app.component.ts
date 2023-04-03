1e488874009fecbe268261f3b593bf9a87a74dc1
Koel Biswas <jbiswako@jci.com>
2023-04-03 15:16:51 +0530

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = 'webpage';
+  title = 'myform';
+
+  fileHandler(event:any) {
+    let currentTarget = (event.target) as HTMLInputElement
+    console.log("Current",currentTarget.files);
+    
+  }
+
+  uploadFile(event:any) {
+
+    let currentTarget = (event.target) as HTMLInputElement
+    if(currentTarget.files) {
+      console.log(currentTarget.files[0]);
+    }
+    console.log("Event ==> ",currentTarget.files);
+    
+  }
 }
