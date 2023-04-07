2769f77e7a32b88d8493a557dca0eb8598056869
Koel Biswas <jbiswako@jci.com>
2023-04-07 17:36:43 +0530

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,63 @@
 import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie/lib/symbols';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
 })
 export class AppComponent {
   title = 'myform';
+  uploadedRs = false;
+  uploadedYu = false;
+  fileType = 'reviewSheet'  // reviewSheet , yumitori
+
+  options: AnimationOptions = {
+    path: '/assets/133744-3d-hologram.json'
+  };
+
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
 
   fileHandler(event:any) {
     let currentTarget = (event.target) as HTMLInputElement
     console.log("Current",currentTarget.files);
-    
   }
 
-  uploadFile(event:any) {
+  uploadFile(event:any,fileTypeName:string) {
 
     let currentTarget = (event.target) as HTMLInputElement
-    if(currentTarget.files) {
-      console.log(currentTarget.files[0]);
+
+    if(currentTarget.files?.length) {
+      const fileEle = document.getElementById(fileTypeName);
+      console.log("Current Class",fileEle);
+      
+      
+      if(fileEle && currentTarget.files[0].type == "text/csv") {
+        fileEle.innerHTML = currentTarget.files[0].name;
+        this.fileType = fileTypeName;
+      }      
     }
-    console.log("Event ==> ",currentTarget.files);
+  }
+
+  drophandler(event:any,fileTypeName:string) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    console.log("File Type Name",fileTypeName);
     
+
+    if(event.dataTransfer.files.length) {
+      const fileEle = document.getElementById(fileTypeName);
+      console.log("Current Class",fileEle);
+
+      
+      if(fileEle && event.dataTransfer.files[0].type == "text/csv") {
+        this.fileType = fileTypeName;
+        fileEle.innerHTML = event.dataTransfer.files[0].name;
+      }      
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LottieModule } from 'ngx-lottie';
+import player from "lottie-web";
+
+
+export function playerFactory() {
+  return player;
+}
 
 @NgModule({
   declarations: [
@@ -11,6 +18,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LottieModule.forRoot({player:playerFactory})
   ],
   providers: [],
   bootstrap: [AppComponent]
